613b818028cda134ae96ff0d0c1db130636f4fd7
Glitch (graphical) <none>
2018-07-14 22:17:34 +0000

    ☠️🐇 Checkpoint ./public/client.js:378534/760

--- public/client.js
+++ public/client.js
@@ -28,11 +28,6 @@ function connect() {
 
     ws.socket.connect.onopen = function () {
       if (ws.socket.connect.readyState === 1) {
-        ws.socket.connect.send(JSON.stringify({
-          type: 'pad.display',
-          message: canvas.toDataURL('image/png')
-        }))
-
         ws.socket.connect.onmessage = function (data) {
           data = JSON.parse(data.data)
           display(data)
@@ -59,56 +54,65 @@ connect()
 
 function display(data) {
   console.log('displaying', data, ws)
+  let img = new Image
+  img.onload = function () {
+     ctx.drawImage(img, 0, 0) 
+  }
+  img.src = data.message
 }
 
 function draw() {
   ctx.lineCap = 'round';
   ctx.globalCompositeOperation = 'screen'
-  ctx.beginPath();
-  ctx.moveTo(prevX, prevY);
-  ctx.lineTo(currX, currY);
-  ctx.strokeStyle = color;
-  ctx.lineWidth = brushWidth;
+  ctx.beginPath()
+  ctx.moveTo(prevX, prevY)
+  ctx.lineTo(currX, currY)
+  ctx.strokeStyle = color
+  ctx.lineWidth = brushWidth
   ctx.stroke()
   ctx.strokeStyle = 'rgb(200, 10, 10)'
   ctx.lineWidth = brushWidth + 2
   ctx.shadowBlur = 15
-  ctx.stroke();
-  ctx.closePath();
+  ctx.stroke()
+  ctx.closePath()
 }
 
 function updateDisplay() {
-  ws.socket.connect.onopen
+  console.log('got here', ws.socket.connect)
+  ws.socket.connect.send(JSON.stringify({
+    type: 'pad.update',
+    message: canvas.toDataURL('image/png')
+  }))
 }
 
 function setDraw() {
-  ctx = canvas.getContext('2d');
+  ctx = canvas.getContext('2d')
 
   function setMove(type, e) {
     if (e.touches) {
-      clientX = e.touches[0].clientX;
-      clientY = e.touches[0].clientY;
+      clientX = e.touches[0].clientX
+      clientY = e.touches[0].clientY
     } else {
-      clientX = e.clientX;
-      clientY = e.clientY;
+      clientX = e.clientX
+      clientY = e.clientY
     }
 
     switch (type) {
       case 'down':
-        prevX = currX;
-        prevY = currY;
-        currX = clientX;
-        currY = clientY;
+        prevX = currX
+        prevY = currY
+        currX = clientX
+        currY = clientY
 
-        flag = true;
-        drawing = true;
+        flag = true
+        drawing = true
 
         if (drawing) {
-          ctx.beginPath();
-          ctx.fillStyle = color;
-          ctx.fillRect(currX, currY, 2, 2);
-          ctx.closePath();
-          drawing = false;
+          ctx.beginPath()
+          ctx.fillStyle = color
+          ctx.fillRect(currX, currY, 2, 2)
+          ctx.closePath()
+          drawing = false
         }
         break;
       case 'up':
@@ -118,10 +122,10 @@ function setDraw() {
         break;
       case 'move':
         if (flag) {
-          prevX = currX;
-          prevY = currY;
-          currX = clientX;
-          currY = clientY;
+          prevX = currX
+          prevY = currY
+          currX = clientX
+          currY = clientY
           draw();
         }
         break;
